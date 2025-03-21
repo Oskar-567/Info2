188a7451b26f9a61b5bc0f38e7b5488acf3ebdc2
Oskar <oskar-bot@gmx.de>
2025-03-21 16:52:37 +0100

    .

--- Vorlesung/006_Quicksort.c
+++ Vorlesung/006_Quicksort.c
@@ -14,22 +14,39 @@ void printArray(int A[], int n)
     printf("\n");
 }
 
+void printPointer(int i, int j)
+{
+  for (int k=0; k < size; k++)
+  {
+    if (k == i || k == j){printf("^ ");}
+    else {printf("  ");}
+  }
+  printf(" changed values \n\n");
+}
+
 int HoarePartition(int A[], int l, int r)
 {
   int x = A[r];
-  int i = l;
-  int j = r;
-  while (i < j)
+  printf("New HoarePartition l:%d , r:%d , x:%d \n", l, r, x);
+  int i = l - 1;
+  int j = r + 1;
+  while (1)
   {
-    while (A[j] > x){j = j - 1;}
-    while (A[i] < x){i = i + 1;}
+    do {j = j - 1;} while (A[j] > x);
+    do {i = i + 1;} while (A[i] < x);
+    printf("i = %d \n", i);
+    printf("j = %d \n", j);
+
     if (i < j)
     {
       int temp = A[i];
       A[i] = A[j];
       A[j] = temp;
+      printArray(A, size);
+      //printf("   changed values: %d & %d\n", A[i], A[j]);
+      printPointer(i, j);
     }
-    else return i;
+    else {return i;}
   }
 }
 
